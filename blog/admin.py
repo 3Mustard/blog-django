21038e8cc1aadde2eda91f46b63949eb793f96aa
3Mustard <m23.cullen@gmail.com>
2020-03-18 22:20:15 -0400

    setup models with admin page. create view functions for blog

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,17 @@
-from django.contrib import admin
+#register models here
 
-# Register your models here.
+from django.contrib import admin
+from blog.models import Post, Category, Comment
+
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+class CommentAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Comment, CommentAdmin)
