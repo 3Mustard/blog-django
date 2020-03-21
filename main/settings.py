52b9a58676850d953069748721f03da2a9d1e7f6
3Mustard <m23.cullen@gmail.com>
2020-03-21 04:57:43 +0000

    all static files moves to root of project. linked a css file with images from the project succesfully

--- main/settings.py
+++ main/settings.py
@@ -120,4 +120,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = "/home/3Mustard/blog-django/static"
+
 STATIC_URL = '/static/'
