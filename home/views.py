800fb30fff7f760300f26eda6a1594aa5b05fc23
3Mustard <m23.cullen@gmail.com>
2020-03-22 01:26:23 +0000

    set up home page with info

--- home/views.py
+++ home/views.py
@@ -2,9 +2,3 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "home_page.html")
-
-def about(request):
-    return render(request, "about.html")
-
-def contact_and_resume(request):
-    return render(request, "contact.html")
